2a615556fa9aa609ee95a36eb364a80b5a566c0a
DuCoy <34075915+Co-lb@users.noreply.github.com>
2020-12-18 12:01:36 +0100

    Modif couleur bar + log Main activty + Fragment Outils Ressources et Brasserie

--- app/src/main/java/com/example/beermanager/MainActivity.java
+++ app/src/main/java/com/example/beermanager/MainActivity.java
@@ -7,15 +7,24 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BeerManagerLogs";
+
     BottomNavigationView btm_view;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG,"On create " + getLocalClassName());
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Outils");
@@ -23,28 +32,59 @@ public class MainActivity extends AppCompatActivity {
         getFragment(new Outils());
 
         btm_view = findViewById(R.id.bottom_view);
-        btm_view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.outils){
-                    getSupportActionBar().setTitle("Outils");
-                    getFragment(new Outils());
 
-                    Toast.makeText(MainActivity.this, "Outils selectionné", Toast.LENGTH_SHORT).show();
-                }else if (item.getItemId() == R.id.brasserie){
-                    getSupportActionBar().setTitle("Brasserie");
-                    getFragment(new Brasserie());
+        //lambda
+        btm_view.setOnNavigationItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.outils){
+                getSupportActionBar().setTitle("Outils");// replace fragment cf getFragment implémenté plus bas
+                getFragment(new Outils());
 
-                    Toast.makeText(MainActivity.this, "Brasserie selectionné", Toast.LENGTH_SHORT).show();
-                }else if (item.getItemId() == R.id.ressources){
-                    getSupportActionBar().setTitle("Ressources");
-                    getFragment(new Ressources());
+                Toast.makeText(MainActivity.this, "Outils selectionné", Toast.LENGTH_SHORT).show(); //PopUp du de l'icone selectionnée
+            }else if (item.getItemId() == R.id.brasserie){
+                getSupportActionBar().setTitle("Brasserie");// replace fragment cf getFragment implémenté plus bas
+                getFragment(new Brasserie());
 
-                    Toast.makeText(MainActivity.this, "Ressources selectionné", Toast.LENGTH_SHORT).show();
-                }
-                return false;
+                Toast.makeText(MainActivity.this, "Brasserie selectionné", Toast.LENGTH_SHORT).show(); //PopUp du de l'icone selectionnée
+            }else if (item.getItemId() == R.id.ressources){
+                getSupportActionBar().setTitle("Ressources");
+                getFragment(new Ressources()); // replace fragment cf getFragment implémenté plus bas
+
+                Toast.makeText(MainActivity.this, "Ressources selectionné", Toast.LENGTH_SHORT).show(); //PopUp du de l'icone selectionnée
             }
+            return false;
         });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"On start " + getLocalClassName());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"On stop " + getLocalClassName());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"On destroy " + getLocalClassName());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"On resume " + getLocalClassName());
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"On restart " + getLocalClassName());
     }
 
     private void getFragment(Fragment fragment) {
